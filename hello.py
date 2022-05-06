2320a9ce54ded77f1d99ce8d4a636bc30d8c36f2
Chansu Park <103991495+Nightcus@users.noreply.github.com>
2022-05-06 13:25:30 +0900

    수정해서 다 넣었다리

--- hello.py
+++ hello.py
@@ -4,9 +4,16 @@
 for i in range(1,20):
     if i%3 == 0:
         print('year')
+    else print(i):
+
 
 for i in range(1:20):
     if i%5 == 0:
         print('dream')
+    else print(i):
 
+for i in range(1:20):
+    if i%15 == 0:
+        print('yeardream')
+    else Print(i):
 
