0ca85ba55d858e633a78191bcfb5a73a2a023576
Chansu Park <103991495+Nightcus@users.noreply.github.com>
2022-05-06 13:21:00 +0900

    :wq

---
+++ hello.py
@@ -0,0 +1,12 @@
+#1부터 10까지 반복하는 과정에서 3의 배수일 경우, year를 출력하시오.
+#나머지는 모든 경우는 숫자 그대로 출력
+
+for i in range(1,20):
+    if i%3 == 0:
+        print('year')
+
+for i in range(1:20):
+    if i%5 == 0:
+        print('dream')
+
+
